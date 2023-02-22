7d1f1f09ff270603398fc16ee766f108c3dd288b
Emilien Marotta <emilien.marotta@gmail.com>
2023-02-22 21:20:26 +0100

    Add CV

--- index.js
+++ index.js
@@ -10,28 +10,28 @@ function change_content(i) {
         let divMiashs = document.getElementsByClassName("about__box__intended-route__miashs")[0];
         if (statusMiashs == 0) {
             statusMiashs = 1;
-            divMiashs.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/back.png" alt="Back" style="width:35px"><span></span><p style='z-index:8;color:#6495ed'><a href="https://idmc.univ-lorraine.fr/parcours-miashs/">MIASHS</a></p><ul ${ulCSS}><li>Mathematics</li><li>Computer Science</li><li>Artificial Intelligence</li><li>Cognitive Science</li><li>Data Bases</li><li>Law</li><li>Economics</li><li>Management</li><li>Information Systems</li></ul>`;
+            divMiashs.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/back.png" alt="Back" style="width:35px" title="Back"><span></span><p style='z-index:8;color:#6495ed'><a href="https://idmc.univ-lorraine.fr/parcours-miashs/">MIASHS</a></p><ul ${ulCSS}><li>Mathematics</li><li>Computer Science</li><li>Artificial Intelligence</li><li>Cognitive Science</li><li>Data Bases</li><li>Law</li><li>Economics</li><li>Management</li><li>Information Systems</li></ul>`;
         } else {
             statusMiashs = 0;
-            divMiashs.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/info.png" alt="Infos" style="width:35px"><span></span><img src="Index/miashs.png" alt="MIASHS"><div class="about__box__intended-route__element__text">MIASHS degree <br>(Bac to Bac+3)</div>`;
+            divMiashs.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/info.png" alt="Infos" style="width:35px" title="Programme"><span></span><img src="Index/miashs.png" alt="MIASHS"><div class="about__box__intended-route__element__text">MIASHS degree <br>(Bac to Bac+3)</div>`;
         }
     } else if(i == 1) {
         let divMiage = document.getElementsByClassName("about__box__intended-route__miage")[0];
         if (statusMiage == 0) {
             statusMiage = 1;
-            divMiage.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/back.png" alt="Back" style="width:35px"><span></span><p style='z-index:8;color:#6495ed'><a href="https://idmc.univ-lorraine.fr/idmc-parcours-miage/">MIAGE</a></p><ul ${ulCSS}><li>Software Engineering</li><li>Cloud Computing</li><li>Data Science</li><li>Artificial Intelligence</li><li>Business Intelligence</li><li>Project Management</li><li>MIS</li><li>Applied Mathematics</li><li>IT Law</li></ul>`;
+            divMiage.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/back.png" alt="Back" style="width:35px" title="Back"><span></span><p style='z-index:8;color:#6495ed'><a href="https://idmc.univ-lorraine.fr/idmc-parcours-miage/">MIAGE</a></p><ul ${ulCSS}><li>Software Engineering</li><li>Cloud Computing</li><li>Data Science</li><li>Artificial Intelligence</li><li>Business Intelligence</li><li>Project Management</li><li>MIS</li><li>Applied Mathematics</li><li>IT Law</li></ul>`;
         } else {
             statusMiage = 0;
-            divMiage.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/info.png" alt="Infos" style="width:35px"><span></span><img src="Index/miage.png" alt="MIAGE"><div class="about__box__intended-route__element__text">MIAGE master's degree <br>(Bac+3 to Bac+5)</div>`;
+            divMiage.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/info.png" alt="Infos" style="width:35px" title="Programme"><span></span><img src="Index/miage.png" alt="MIAGE"><div class="about__box__intended-route__element__text">MIAGE master's degree <br>(Bac+3 to Bac+5)</div>`;
         }
     } else {
         let divMines = document.getElementsByClassName("about__box__intended-route__mines")[0];
         if (statusMines == 0) {
             statusMines = 1;
-            divMines.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/back.png" alt="Back" style="width:35px"><span></span><p style='z-index:8;color:#6495ed'><a href="https://mines-nancy.univ-lorraine.fr/formation/mastere-specialise-cybersecurite-attaque-defense-des-systemes-informatiques/">Cybersecurity</a></p><ul ${ulCSS}><li>Malware</li><li>Reverse Engineering</li><li>Network Security</li><li>Cryptography</li><li>Crisis Management</li><li>Security Policy</li><li>Communications</li><li>Forensic</li><li>OS & Virtualisation</li></ul>`;
+            divMines.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/back.png" alt="Back" style="width:35px" title="Back"><span></span><p style='z-index:8;color:#6495ed'><a href="https://mines-nancy.univ-lorraine.fr/formation/mastere-specialise-cybersecurite-attaque-defense-des-systemes-informatiques/">Cybersecurity</a></p><ul ${ulCSS}><li>Malware</li><li>Reverse Engineering</li><li>Network Security</li><li>Cryptography</li><li>Crisis Management</li><li>Security Policy</li><li>Communications</li><li>Forensic</li><li>OS & Virtualisation</li></ul>`;
         } else {
             statusMines = 0;
-            divMines.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/info.png" alt="Infos" style="width:35px"><span></span><img src="Index/mines.png" alt="Cybersecurity"><div class="about__box__intended-route__element__text">Specialised master's degree in Cybersecurity <br>(Bac+5 to Bac+6)</div>`;
+            divMines.innerHTML = `<img onclick="change_content(${i})" class="about__box__intended-route__element__button" src="Index/info.png" alt="Infos" style="width:35px" title="Programme"><span></span><img src="Index/mines.png" alt="Cybersecurity"><div class="about__box__intended-route__element__text">Specialised master's degree in Cybersecurity <br>(Bac+5 to Bac+6)</div>`;
         }
     }
 }
